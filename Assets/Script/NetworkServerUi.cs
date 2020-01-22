ba3d7a6bce3ff8455633052a63d9b2b02853f8e2
Testing <yeekhei@gmail.com>
2020-01-23 00:01:41 +0800

    First Comment
    A

---
+++ Assets/Script/NetworkServerUi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class NetworkServerUi : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        //GUI.Box(new Rect(10, Screen.height - 35, 100, 20), "Status :" + NetworkServer.active);
+        //.Box(new Rect(20, Screen.height - 20, 100, 20), "Connected :" + NetworkServer.connections.Count);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NetworkServer.Listen(25000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/collisionScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject explosion = Instantiate(Resources.Load("DustExplosion", typeof(GameObject))) as GameObject;
+        explosion.transform.position = transform.position;
+        Destroy(other.gameObject);
+        Destroy(explosion, 2);
+
+        if(GameObject.FindGameObjectsWithTag("Player").Length==0)
+        {
+            GameObject enemy1=Instantiate(Resources.Load("enemy 1",typeof(GameObject)))as GameObject;
+            GameObject enemy2=Instantiate(Resources.Load("enemy 2",typeof(GameObject)))as GameObject;
+            GameObject enemy3=Instantiate(Resources.Load("enemy 3",typeof(GameObject)))as GameObject;
+            GameObject enemy4=Instantiate(Resources.Load("enemy 4",typeof(GameObject)))as GameObject;
+        }
+        Destroy(gameObject);
+    }
+}
